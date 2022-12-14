7de740a00045a10477b2aebc3affc52b362d7e28
krzeu <anthonykoria@live.se>
2022-12-14 23:43:27 +0100

    horror rooms lvl 1

---
+++ LeftOrRight/Assets/_LeftOrRight/Scripts/FlashLight.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public Material lens;
+
+    private Light _light;
+    private AudioSource _audioSource;
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void LightOn()
+    {
+        _audioSource.Play();
+        lens.EnableKeyword("_Emission");
+        _light.enabled = true;
+    }
+
+    public void LightOff()
+    {
+        _audioSource.Play();
+        lens.DisableKeyword("_Emission");
+        _light.enabled = false;
+    }
+}
